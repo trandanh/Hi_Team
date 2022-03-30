477cc59ef1eed062a8b3f3546ca8e62baedd3b28
tai01112000 <td.tai2000@gmail.com>
2022-03-30 09:38:57 +0700

    Update InputHandler: Liên kết CameraHandler di chuyển theo sau nhân vật

--- InputHandler.cs
+++ InputHandler.cs
@@ -14,9 +14,25 @@ namespace Hi_Team
         public float mouseY;
 
         PlayerControls inputActions;
+        CameraHandler cameraHandler;
 
         Vector2 movementInput;
         Vector2 cameraInput;
+
+        private void Awake()
+        {
+            cameraHandler = CameraHandler.singleton;
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandlerCameraRotation(delta, mouseX, mouseY);
+            }
+        }
 
         public void OnEnable()
         {
