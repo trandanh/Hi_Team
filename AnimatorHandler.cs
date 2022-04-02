6387391c84094f13910a4a9ce3a4bb64d59f0757
Pham Van Khanh <khanhphamvan1910@gmail.com>
2022-04-02 21:10:36 +0700

    khánh commit

--- AnimatorHandler.cs
+++ AnimatorHandler.cs
@@ -7,6 +7,8 @@ namespace Hi_Team
     public class AnimatorHandler : MonoBehaviour
     {
         public Animator anim;
+        public InputHandler inputHandler;
+        public PlayerLocomotion playerLocomotion;
         int vertical;
         int horizontal;
         public bool canRotate;
@@ -14,11 +16,13 @@ namespace Hi_Team
         public void Initialize()
         {
             anim = GetComponent<Animator>();
+            inputHandler = GetComponentInParent<inputHandler>();
+            playerLocomotion = GetComponentInParent<playerLocomotion>();
             vertical = Animator.StringToHash("Vertical");
             horizontal = Animator.StringToHash("Horizontal");
         }
 
-        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement, bool isSprinting)
         {
             #region Vertical
             float v = 0;
@@ -70,8 +74,21 @@ namespace Hi_Team
             }
             #endregion
 
+            if (isSprinting)
+            {
+                v = 2;
+                h = horizontalMovement;
+            }
+
             anim.SetFloat(vertical, v, 0.1f, Time.deltaTime);
             anim.SetFloat(horizontal, h, 0.1f, Time.deltaTime);
+        }
+
+        public void PlayTargetAnimation(string targetAnim, bool isInteracing)
+        {
+            anim.applyRootMotion = isInteracing;
+            anim.SetBool("isInteracing", isInteracing);
+            anim.CrossFade(targetAnim, 0.2f);
         }
 
         public void CanRotate()
@@ -83,6 +100,17 @@ namespace Hi_Team
         {
             canRotate = false;
         }
-    
+
+        private void OnAnimatorMove()
+        {
+            return;
+
+            playerLocomotion.rigibody.drag = 0;
+            Vector3 deltaPosition = anim.deltaPosition;
+            deltaPosition.y = 0;
+            Vector3 velocity = deltaPosition / delta;
+            playerLocomotion.rigibody.velocity = velocity;
+        }
+
     }
 }
