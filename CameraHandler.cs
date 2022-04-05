7627fe742708244a15ea54cbf3a0cae37927faac
TranDanh <trandanh364@gmail.com>
2022-04-06 01:27:58 +0700

    commit check

--- CameraHandler.cs
+++ CameraHandler.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Hi_Team
 {
 	public class CameraHandler : MonoBehaviour
 	{
--- InputHandler.cs
+++ InputHandler.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Hi_Team
 {
     public class InputHandler : MonoBehaviour
     {
